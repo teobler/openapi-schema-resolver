a77609751535e046a071d369ce26b9745cdc8780
teobler <teobler@163.com>
2021-10-03 10:58:22 +0800

    delete tsc types after build.

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,6 +4,7 @@ import rollupBabel from "rollup-plugin-babel";
 // @ts-ignore
 import rollupTypeScript from "rollup-plugin-typescript";
 import dts from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
 
 const pkg = require(path.join(__dirname, "package.json"));
 
@@ -11,14 +12,8 @@ module.exports = [
   {
     input: "./src/index.ts",
     output: [
-      {
-        file: pkg.main,
-        format: "cjs",
-      },
-      {
-        file: pkg.module,
-        format: "es",
-      },
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
     ],
     external: [
       "tslib",
@@ -41,7 +36,11 @@ module.exports = [
   },
   {
     input: "./dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts()],
+    output: [
+      { file: "dist/index.d.ts", format: "umd" },
+      { file: "lib/index.d.ts", format: "cjs" },
+      { file: "module/index.d.ts", format: "es" },
+    ],
+    plugins: [dts(), del({ targets: "dist/types", hook: "buildEnd" })],
   },
 ];
